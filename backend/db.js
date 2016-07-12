82ebf37bb467d8da88c9fe1e810e6f2533ed552c
Yevgeny Nikitin <jeremy.nikitin@retroficiency.com>
2016-07-12 15:19:32 -0400

    move schema

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     port     : 3306,
     user     : 'root',
     password : 'pikachu',
-    database : 'lunches'
+    database : 'funch'
 });
 
 connection.connect();
