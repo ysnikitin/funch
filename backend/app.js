3c19cf59eaf0b0d68934c0bb3f49f088a66051fb
Yevgeny Nikitin <jeremy.nikitin@retroficiency.com>
2016-07-13 17:59:51 -0400

    cors

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 // register routes
 app.use('/', routes);
 
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
